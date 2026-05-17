268a5143db2e86a55b92d3e44151b94cf8c55d50
Activos100x100 <it@activos100x100.com>
2026-05-17 07:56:03 +0200

    nuevo cambio

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
+
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+
+from app.qnla.routers import pages
 
 app = FastAPI()
 templates = Jinja2Templates(directory="app/templates")
@@ -8,3 +11,6 @@ templates = Jinja2Templates(directory="app/templates")
 @app.get("/", response_class=HTMLResponse)
 def root(request: Request):
     return templates.TemplateResponse("qnla/admin_torneos.html", {"request": request})
+
+# Incluir el router de páginas de QNLA
+app.include_router(pages.router)
