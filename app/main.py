c659ceb8984ea2f736e286576cb71df28dbfb494
Activos100x100 <it@activos100x100.com>
2026-05-17 07:53:09 +0200

    nuevo cambio

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="app/templates")
 
-@app.get("/")
-def root():
-    return {"message": "QNLA funcionando"}
+@app.get("/", response_class=HTMLResponse)
+def root(request: Request):
+    return templates.TemplateResponse("qnla/admin_torneos.html", {"request": request})
